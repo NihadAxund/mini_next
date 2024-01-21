bc377d77c64ae889a17b48196051a310a7a69077
NihadAxund <coinnihad@gmail.com>
2024-01-21 19:57:17 +0400

    first commit

---
+++ app/home/page.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import TestBtn from "../components/TestBtn";
+
+export default function Home() {
+    return(
+        <main>
+            <header>
+
+            </header>
+            <div className="thread">
+                <TestBtn></TestBtn>
+            </div>
+        </main>
+    )
+}
+
+function  lalal() {
+    alert("a")
+}
